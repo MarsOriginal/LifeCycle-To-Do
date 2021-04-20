c1aed564099a156aec18d9d6c39c605cf48e66ff
Zheng Li(MarsOriginal) <zlii0145@student.monash.edu>
2021-04-20 15:33:20 +1000

    Initial Commit

---
+++ LifeCycle-To-Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle-To-Do
+//
+//  Created by MARS on 20/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
